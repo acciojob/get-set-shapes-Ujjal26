1136eef54d7d45b9028ef5e67924dc471241c7d0
Ujjal26 <156526573+Ujjal26@users.noreply.github.com>
2026-03-02 05:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 //complete this code
 class Rectangle {
-	constructor(width, height){
-		this._width = width;
-		this._height = height;
-	}
-	get width(){
-		return this._width;
-	}
-	get height(){
-		return this._height;
-	}
-	function getArea(){
-		return this.width * this.height;
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
 }
 
 class Square extends Rectangle {
-	constructor(side){
-		super(side, side);
-	}
-	function getPerimeter(){
-		return 4*this.width;
-	}
+  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
+// Attach to window for Cypress tests if in a browser environment
+if (typeof window !== 'undefined') {
+  window.Rectangle = Rectangle;
+  window.Square = Square;
 }
 
 // Do not change the code below this line
